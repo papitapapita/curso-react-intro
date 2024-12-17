f6e27efe3b2df6e43e2dc866cbf9486076f8b113
Jonathan <many.d.r.o.2001@hotmail.com>
2024-12-17 15:26:17 -0500

    --amend

---
+++ src/AddToDoButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AddToDoButton.css';
+
+export function CreateToDoButton() {
+  return (
+    <button className="add-button" type="button" value="+">
+      +
+    </button>
+  );
+}
