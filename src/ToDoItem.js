dbfc3b4a6e2cc7de913b03cd3913999c37035678
Jonathan <many.d.r.o.2001@hotmail.com>
2024-12-17 22:21:34 -0500

    ToDoItems styles created with CompleteIcon and DeleteIcon files

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,11 +1,31 @@
-function ToDoItem(props) {
+import './ToDoItem.css';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { DeleteIcon } from './DeleteIcon';
+import { CompleteIcon } from './CompleteIcon';
+
+function ToDoItem({
+  description,
+  completed,
+  onComplete,
+  onDelete,
+}) {
   return (
-    <li>
-      <span>✅</span>
-      <p>{props.description}</p>
-      <span>❌</span>
+    <li
+      className={`to-do ${completed ? 'to-do--completed' : ''}`}
+    >
+      <CompleteIcon onComplete={onComplete} />
+      <p className="to-do__text">{description}</p>
+      <DeleteIcon onDelete={onDelete} />
     </li>
   );
 }
+
+ToDoItem.propTypes = {
+  description: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  onComplete: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export { ToDoItem };
