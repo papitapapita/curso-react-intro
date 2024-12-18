7b93d1f56867784fc9a89f35975ca5d1a9ee1ac2
Jonathan <many.d.r.o.2001@hotmail.com>
2024-12-17 22:04:53 -0500

    Creation of To Do Counter styles and some improvements

--- src/ToDoCounter.js
+++ src/ToDoCounter.js
@@ -1,7 +1,25 @@
-export function ToDoCounter(props) {
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ToDoCounter.css';
+
+export function ToDoCounter({ completed, total }) {
+  const isCompleted = completed === total;
   return (
-    <h1>
-      {props.completed} completed from {props.total}
+    <h1 className="title">
+      {isCompleted ? (
+        <b className="title__completed">
+          You have completed everything
+        </b>
+      ) : (
+        <span>
+          You have completed {completed} of {total} tasks
+        </span>
+      )}
     </h1>
   );
 }
+
+ToDoCounter.propTypes = {
+  completed: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+};
